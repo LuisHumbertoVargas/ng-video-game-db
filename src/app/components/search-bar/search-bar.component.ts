1b5ffd592b0a62f5c0eaedf26da0e59541440e1d
Luis <luish.vargas09@gmail.com>
2024-06-17 00:46:18 -0600

    Creating routes

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss'],
+  styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    this.router.navigate(['search', form.value.search]);
+  }
+
 }
