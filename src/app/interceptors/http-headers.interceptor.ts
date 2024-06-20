f8b0b2b78b16741b4bddcd29e045177253124c98
LuisVargas <luish.vargas09@gmail.com>
2024-06-19 22:36:50 -0600

    Implement dinamic list games with your data

--- src/app/interceptors/http-headers.interceptor.ts
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -5,19 +5,20 @@ import { Observable } from "rxjs";
 @Injectable()
 export class HttpHeadersInterceptor implements HttpInterceptor {
     constructor() {}
+    
     intercept(
         req: HttpRequest<any>, 
         next: HttpHandler
     ): Observable<HttpEvent<any>> {
         req = req.clone({
             setHeaders: {
-                'X-RapidAPI-Key': '4d2fd06bb0mshd240c3ae8285e0ep163b17jsn9456c953005c',
-                'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+                'X-Rapidapi-Key': 'e037efce75msh707cf9cc231d92bp104206jsn47c91ab9fd7a',
+                'X-Rapidapi-Host': 'rawg-video-games-database.p.rapidapi.com',
         },
         setParams: {
-            key: '4d2fd06bb0mshd240c3ae8285e0ep163b17jsn9456c953005c'
+            key: 'd9e6ecbe51e8499fa9eb9182843d586b',
         }
     });
-        return next.handle(req);
+    return next.handle(req);
     }
 }
